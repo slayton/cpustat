18ed6838efcaaae7751f38dbbef3e37b16abb601
Stuart Layton <stuart.layton@gmail.com>
2012-10-24 20:35:58 -0400

    initial commit of the cpustat project. It currently polls for active CPU percentage and percent ram used

--- cpustat/AppDelegate.h
+++ cpustat/AppDelegate.h
@@ -8,8 +8,31 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+    
+    float timeOut;
+    bool timerRunning;
 
+    processor_info_array_t cpuInfo, prevCpuInfo;
+    mach_msg_type_number_t numCpuInfo, numPrevCpuInfo;
+    unsigned numCPUs;
+    NSTimer *updateTimer;
+    NSLock *CPUUsageLock;
+}
 @property (assign) IBOutlet NSWindow *window;
+
+@property (nonatomic) float timeOut;
+@property (nonatomic) bool timerRunning;
+
+-(void) startTimer;
+-(void) timerExpired;
+-(float) getCpuPercentUsasge;
+-(float) getMemPercentFree;
+-(float) getNetUsage;
+
+
+
+
 
 @end
--- cpustat/AppDelegate.m
+++ cpustat/AppDelegate.m
@@ -7,12 +7,124 @@
 //
 
 #import "AppDelegate.h"
+#import <sys/sysctl.h>
+#import <mach/host_info.h>
+#import <mach/mach_host.h>
+#import <mach/task_info.h>
+#import <mach/task.h>
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    timerRunning = false;
+    timeOut = 1;
+    NSLog(@"Application Loaded!");
+    
+    int mib[2U] = { CTL_HW, HW_NCPU };
+    size_t sizeOfNumCPUs = sizeof(numCPUs);
+    int status = sysctl(mib, 2U, &numCPUs, &sizeOfNumCPUs, NULL, 0U);
+    
+    if(status)
+        numCPUs = 1;
+    
+    CPUUsageLock = [[NSLock alloc] init];
+    
+    [self startTimer];
+    
+}
+
+-(void) startTimer{
+    NSLog(@"Starting the timer!");
+    [NSTimer scheduledTimerWithTimeInterval: timeOut
+                                     target: self
+                                   selector:@selector(timerExpired)
+                                   userInfo: nil
+                                    repeats: YES];
+}
+
+-(void) timerExpired{
+    NSLog(@"timer expired");
+    float cpu = [self getCpuPercentUsasge];
+    float mem = [self getMemPercentFree];
+    float net = [self getNetUsage];
+}
+
+-(float) getCpuPercentUsasge{
+    natural_t numCPUsU = 0U;
+
+    kern_return_t err = host_processor_info(mach_host_self(), PROCESSOR_CPU_LOAD_INFO, &numCPUsU, &cpuInfo, &numCpuInfo);
+
+    if(err == KERN_SUCCESS) {
+        [CPUUsageLock lock];
+        NSLog(@"getCpuPercentUsage");
+
+        for(unsigned i = 0U; i < numCPUs; ++i) {
+            float inUse, total;
+            if(prevCpuInfo) {
+                inUse = (
+                         (cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_USER]   - prevCpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_USER])
+                         + (cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_SYSTEM] - prevCpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_SYSTEM])
+                         + (cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_NICE]   - prevCpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_NICE])
+                         );
+                total = inUse + (cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_IDLE] - prevCpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_IDLE]);
+            } else {
+                inUse = cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_USER] + cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_SYSTEM] + cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_NICE];
+                total = inUse + cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_IDLE];
+            }
+            
+            NSLog(@"Core: %u Usage: %f",i,inUse / total);
+        }
+        [CPUUsageLock unlock];
+        
+        if(prevCpuInfo) {
+            size_t prevCpuInfoSize = sizeof(integer_t) * numPrevCpuInfo;
+            vm_deallocate(mach_task_self(), (vm_address_t)prevCpuInfo, prevCpuInfoSize);
+        }
+        
+        prevCpuInfo = cpuInfo;
+        numPrevCpuInfo = numCpuInfo;
+        
+        cpuInfo = NULL;
+        numCpuInfo = 0U;
+    } else {
+        NSLog(@"Error!");
+        [NSApp terminate:nil];
+    }
+    return 1;
+}
+-(float) getMemPercentFree{
+    
+    int mib[6];
+    mib[0] = CTL_HW;
+    mib[1] = HW_PAGESIZE;
+    
+    int pagesize;
+    size_t length;
+    length = sizeof (pagesize);
+    if (sysctl (mib, 2, &pagesize, &length, NULL, 0) < 0)
+    {
+        fprintf (stderr, "getting page size");
+    }
+    
+    mach_msg_type_number_t count = HOST_VM_INFO_COUNT;
+    
+    vm_statistics_data_t vmstat;
+    if (host_statistics (mach_host_self (), HOST_VM_INFO, (host_info_t) &vmstat, &count) != KERN_SUCCESS)
+    {
+        fprintf (stderr, "Failed to get VM statistics.");
+    }
+    
+    double total = vmstat.wire_count + vmstat.active_count + vmstat.inactive_count + vmstat.free_count;
+    double perFree = (vmstat.free_count + vmstat.inactive_count) / total;
+    
+    NSLog( [NSString stringWithFormat:@"PercentMemFree:%3.3f\n",  perFree]);
+
+    return perFree;
+}
+-(float) getNetUsage{
+    return 1;
 }
 
 @end
